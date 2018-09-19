3dcd06259993b454924367879c5977532443f990
Tim Allen <tim@noblesamurai.com>
2018-09-19 13:21:44 +1000

    Finess output of func.

--- index.js
+++ index.js
@@ -14,13 +14,13 @@ module.exports = async (filePath) => {
   const metadata = await ffprobe(filePath);
   const { width, height } = metadata.streams.find(s => s.codec_type === 'video');
   const { format_name: formatName } = metadata.format;
-  let format;
+  let mime;
   const [formatNameMatch] = formatName.match(/(webm|ogg|mp4)/);
   if (formatNameMatch === 'mp4') {
-    format = await mpeg4MimeType(filePath);
+    mime = await mpeg4MimeType(filePath);
   } else {
     const codecs = metadata.streams.map((s) => s.codec_name).filter(c => c !== 'unknown');
-    format = `video/${formatNameMatch}; codecs="${codecs.join(', ')}"`;
+    mime = `video/${formatNameMatch}; codecs="${codecs.join(', ')}"`;
   }
-  return { width: parseInt(width), height: parseInt(height), format, ffprobe: metadata };
+  return { width: parseInt(width), height: parseInt(height), format: formatNameMatch, mime, ffprobe: metadata };
 };
