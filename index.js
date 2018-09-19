32bab89e6f63d42cf9f2ceadbd08c6e04bb51e43
Tim Allen <tim@noblesamurai.com>
2018-09-19 11:17:36 +1000

    Initial cut.

---
+++ index.js
@@ -0,0 +1,18 @@
+const mpeg4MimeType = require('mpeg4-mimetype');
+const ffmpeg = require('fluent-ffmpeg');
+const util = require('util');
+const ffprobe = util.promisify(ffmpeg.ffprobe);
+ffmpeg.setFfprobePath(require('ffprobe-static').path);
+
+module.exports = async (filePath) => {
+  const metadata = await ffprobe(filePath);
+  const { width, height } = metadata.streams.find(s => s.codec_type === 'video');
+  const { tags: { compatible_brands: compatibleBrands } } = metadata.format;
+  let format;
+  if (compatibleBrands && compatibleBrands.match(/mp4/)) {
+    format = await mpeg4MimeType(filePath);
+  } else {
+    format = 'blerg';
+  }
+  return { width, height, format, ffprobe: metadata };
+};
--- test/index.js
+++ test/index.js
@@ -1,8 +1,24 @@
 const expect = require('chai').expect;
+const videoMimeType = require('..');
+const path = require('path');
+const examples = [
+  {
+    filename: 'SampleVideo.mp4',
+    expectation: 'etc'
+  },
+  {
+    filename: 'big-buck-bunny_trailer.webm',
+    expectation: 'etc'
+  }
+];
 
-describe('my thing', function () {
-  it('should work', function () {
-    expect(true).to.be.true;
-    throw new Error('unimplemented');
+describe('video-mimetype', function () {
+  it('should give a mimetype for mp4 files', function () {
+    return Promise.all(examples.map(async (example) => {
+      const result = await videoMimeType(path.join(__dirname, 'fixtures', example.filename));
+      expect(result).to.be.an('object');
+      expect(result).to.have.keys('width', 'height', 'format', 'ffprobe');
+      expect(result.format).to.match(/video\/mpeg4/);
+    }));
   });
 });
